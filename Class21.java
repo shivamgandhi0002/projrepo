fd19ff8d19de067fdc0f723bbd4dbbe5befd3f1b
shivamgandhi0002 <118705357+shivamgandhi0002@users.noreply.github.com>
2022-12-14 21:22:26 -0500

    Add files via upload

---
+++ Class21.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Class21 {
+
+	public static void main(String[] args) {
+		Circle12 circle12 = new Circle12(5);
+		
+		System.out.println("Area   : "+circle12.getArea());
+		System.out.println("Perimeter   : "+circle12.getPerimeter());
+		
+
+	}
+
+}
